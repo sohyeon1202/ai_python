9bad0674efe1ac8413d6c1252dc2321748b72af8
sohyeon1202 <163490049+sohyeon1202@users.noreply.github.com>
2024-03-15 12:07:42 +0900

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,11 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:/Users/User/Desktop/1.py =====================
+1 2
+3
+>>> 
+====================== RESTART: C:/Users/User/Desktop/1.py =====================
+두 정수를 입력하세요(띄어쓰기로 구분): 1 2
+두 정수의 합은: 3
+>>> 
